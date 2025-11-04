ab0fee2f1a47261422e865afc2604761528550b1
João Gonçalves <129080033+joaogoncalves-dev@users.noreply.github.com>
2025-11-04 14:02:35 -0300

    Dia 4 concluído: Fundamentos de
    Entrada de Dados (Input).
    
    Hoje aprendemos a receber e imprimir dados via GUI
    
    amanhâ vamos a defender o código com try-catch e while.

---
+++ src/br/com/joao/fundamentos/GuiInputOutput.java
@@ -0,0 +1,45 @@
+package br.com.joao.fundamentos;
+
+import javax.swing.JOptionPane;
+
+public class GuiInputOutput {
+	
+public static void main(String[]args) {
+		
+		// dia 4: - Input e Output no JOption
+		// hoje vamos focar em entender como funciona a Entrada de dados,
+		// amanhâ aprenderemos como deixar o código intacto contra
+	    // erros de usuários ao receber dados
+		
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		   //receber e imprimir dados via GUI ()JOptionPane - janelas de diálago)
+		
+		
+		   // primeiro importamos a biblioteca  javax.swing.JOptionPane; com um import
+		
+		   // receber e imprimir dados do tipo string 
+		
+		String nome = JOptionPane.showInputDialog(null, "digite seu nome: ");
+	    
+		JOptionPane.showMessageDialog(null, "olá " + nome);
+	    
+	       // receber e imprimir dados do tipo int
+	    
+		String inputIdade = JOptionPane.showInputDialog(null, "digite sua idade: ");
+		int idade = Integer.parseInt(inputIdade);
+	    
+		JOptionPane.showMessageDialog(null, "Sua idade é: " + idade);
+	 
+	       // receber e imprimir dados do tipo double (GUI)
+	    
+		String inputNota = JOptionPane.showInputDialog(null, "digite sua nota: ");
+		double nota = Double.parseDouble(inputNota);
+	    
+		JOptionPane.showMessageDialog(null, "sua nota é: " + nota);
+		
+	}
+
+}
